4c497d08a4affb54e06908fcfa1b341d674f1cbb
adm-multisaberonline <adm.multisaberonline@gmail.com>
2025-06-17 17:28:44 -0300

    Create a Spring Project blog

---
+++ src/test/java/local/mso/app/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package local.mso.app.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
